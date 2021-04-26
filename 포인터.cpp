712c76c68d9665f29087cb91d8621bc55d052bfd
sinakim1 <81377561+sinakim1@users.noreply.github.com>
2021-04-26 17:20:21 +0900

    Add files via upload

---
+++ 포인터.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int* ptr; //포인터 선언하기
+	ptr = &a; //포인터 초기화 하기
+
+	cout << "변수 a의 주소는 :" << ptr << endl; //주소 출력
+	cout << "a의 값은: " << a << endl; //a의 값 출력
+	return 0;
+
+}
